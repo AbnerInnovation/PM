50a8ee7c3b1a9afed4470849fcc208fac36fb7d2
AbnerInnovation <luisabnersoto@gmail.com>
2016-10-18 15:29:54 -0700

    Hello Commit

---
+++ app/src/main/java/mx/com/isc/itsc/hp_abner/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.com.isc.itsc.hp_abner.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
